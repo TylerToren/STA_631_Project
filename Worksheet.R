dd18b77ea3db726aee636f065acf0047dcce708a
Mageshwaran Anbazhagan <mickey@MageshwaransAir.bloombb.com>
2023-03-27 09:23:25 -0400

    Magesh Changes

---
+++ Worksheet.R
@@ -0,0 +1,110 @@
+# Load necessary packages
+library(dplyr)
+library(ggplot2)
+library(corrplot)
+library(Lahman)
+
+# Load batting data from Lahman Baseball Database
+batting <- read.csv("Batting.csv") %>% 
+  select(playerID,yearID,stint,teamID,lgID,G,AB,R,H,X2B,X3B,HR) %>% 
+  rename("Player_ID" = playerID,
+         "Year_ID" = yearID,
+         "Stint" = stint,
+         "Team" = teamID,
+         "League" = lgID,
+         "Games_Played" = G,
+         "At_Bats" = AB,
+         "Runs" = R,
+         "Hits" = H,
+         "Doubles" = X2B,
+         "Triples" = X3B,
+         "Home_Runs" = HR)
+people <- read.csv("People.csv") %>% 
+  select(playerID,birthYear,birthMonth,birthDay,birthCountry,birthState,birthCity,nameFirst,nameLast,nameGiven,weight,height,bats,throws,debut,finalGame) %>% 
+  rename("Player_ID" = playerID,
+         "Birth_Year" = birthYear,
+         "Birth_Month" = birthMonth,
+         "Birth_Day" = birthDay,
+         "Birth_Country" = birthCountry,
+         "Birth_State" = birthState,
+         "Birth_City" = birthCity,
+         "First_Name" = nameFirst,
+         "Last_Name" = nameLast,
+         "Given_Name" = nameGiven,
+         "Weight" = weight,
+         "Height" = height,
+         "Batting_Hand" = bats,
+         "Throwing_Hand" = throws,
+         "Debut" = debut,
+         "Final_Game" = finalGame)
+
+fielding <- read.csv("Fielding.csv") %>% 
+  select(playerID,POS) %>% 
+  rename("Player_ID" = playerID,
+         "Position" = POS)
+
+salaries <- read.csv("Salaries.csv") %>% 
+  select(playerID, salary) %>% 
+  rename("Player_ID" = playerID,
+         "Salary" = salary)
+
+collegeplaying <- read.csv("CollegePlaying.csv") %>% 
+  select(playerID, schoolID) %>% 
+  rename("Player_ID" = playerID,
+         "School_Playing" = schoolID)
+
+school <- read.csv("Schools.csv") %>% 
+  select(schoolID,name_full) %>% 
+  rename("School_Playing" = schoolID,
+         "School_Name" = name_full)
+
+awards <- read.csv("AwardsPlayers.csv") %>% 
+  select(playerID,awardID) %>% 
+  rename("Player_ID" = playerID,
+         "Awards" = awardID)
+
+#Merging all dataset to get one final dataset
+final_data <- merge(batting, people, by = "Player_ID")
+final_data <- merge(final_data, fielding, by = "Player_ID")
+final_data <- merge(final_data, salaries , by = "Player_ID")
+final_data <- merge(final_data, collegeplaying, by = "Player_ID")
+final_data <- merge(final_data, school, by = "School_Playing")
+final_data <- merge(final_data, awards, by = "Player_ID")
+
+
+# Create new variable for batting average
+final_data$Average <- ifelse(is.nan(final_data$Hits / final_data$At_Bats),0,final_data$Hits / final_data$At_Bats)
+
+# Remove missing data
+final_data <- na.omit(final_data)
+
+
+#Create a new variable for age
+final_data$Current_Age <- as.integer((Sys.Date() - as.Date(paste(final_data$Birth_Year, final_data$Birth_Month, final_data$Birth_Day, sep = "-"))) / 365.25)
+final_data$Debut_Age <- as.integer((Sys.Date() - as.Date(final_data$Debut)) / 365.25)
+final_data$FinalGame_Age <- as.integer((Sys.Date() - as.Date(final_data$Final_Game)) / 365.25)
+
+
+# Create a correlation matrix to identify potential predictors
+cor(final_data[,c("Average", "At_Bats", "Hits", "Games_Played")])
+
+#Create a model
+model <- lm(Average ~ School_Playing, data = final_data %>% filter(At_Bats > 30))
+# Print summary of model results
+summary(model)
+
+# Create scatterplot of predicted versus actual batting average
+predicted <- predict(model, newdata = final_data %>% filter(At_Bats > 30))
+ggplot(final_data %>% filter(At_Bats > 30), aes(x = Average, y = predicted)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  labs(x = "Actual Batting Average", y = "Predicted Batting Average")
+
+# # Calculate cross-validated prediction error
+# library(caret)
+# set.seed(123)
+# cv <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
+# model_cv <- train(Average ~ Games_Played + At_Bats + Hits, data = final_data %>% filter(At_Bats > 30), method = "lm", trControl = cv)
+# model_cv$results$RMSE
+
+
